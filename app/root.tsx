4973c14fb48de330c36a6b0ba761c36349d922ba
Sethfire <17826826+Sethfire@users.noreply.github.com>
2025-10-07 23:46:47 +1100

    update favicon

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/castle3.png" />
         <Meta />
         <Links />
       </head>
